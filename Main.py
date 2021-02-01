7acb2d0dc651db35fe22136638ec33b2e5a81dc5
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2021-02-01 10:32:46 +0100

    graphics: try to modularize

--- Main.py
+++ Main.py
@@ -1,104 +1,14 @@
-from graphicsnode.NodeDraw import NodeDraw
-from graphicsnode.Colors import Color
 from algorithms.BFS import bfs
 from files.GraphGetter import GraphGetter
 from graphics import *
-from structure.Pairs import Pairs
 from graphicsnode.Dimensions import Dimensions
-from graphicsnode.PositionChecker import PositionChecker
-import random
+from graphicsnode.PaintGraph import PaintGraph
 
+win = GraphWin("Graphs PlayGround", Dimensions.SCREEN_WIDTH.value, Dimensions.SCREEN_HEIGHT.value)
+
+# Graph and BFS of all vertices
 graph = GraphGetter.get_graph_from_file()
-bfs_tree = bfs(graph, "A")
-
-# Variable for dimensions
-circle_radius = Dimensions.CIRCLE_RADIUS.value
-screen_width = Dimensions.SCREEN_WIDTH.value
-screen_height = Dimensions.SCREEN_HEIGHT.value
-safety = Dimensions.SAFETY_VALUE.value
-
-
-def main():
-    win = GraphWin("Graphs PlayGround", screen_width, screen_height)
-    node_draws = []
-    point_history = []
-    edges = []
-
-    # Save node draw objects in node draws list
-    for current_node in graph.nodes:
-        point = None
-        while PositionChecker.not_valid(point, point_history):
-            random_x = random.randint(1, screen_width)
-            random_y = random.randint(1, screen_height)
-            x_point = (screen_width - 2 * circle_radius - safety + random_x) % (screen_width - 2 * circle_radius)
-            y_point = (screen_height - 2 * circle_radius - safety + random_y) % (screen_height - 2 * circle_radius)
-            point = Point(x_point + circle_radius, y_point + circle_radius)
-        point_history.append(point)
-        node_draws.append(NodeDraw((Circle(point, circle_radius)), Text(point, current_node.value), current_node.value))
-
-    # Add node draw objects to canvas
-    for graphic_node in node_draws:
-        graphic_node.circle.setFill(Color.RED.value)
-        graphic_node.circle.draw(win)
-        graphic_node.text.setTextColor(Color.BLACK.value)
-        graphic_node.text.draw(win)
-
-    # Get edges
-    for current_node in graph.nodes:
-        for adjacent in current_node.adjacent_nodes:
-            current_pair = Pairs(current_node.value, adjacent.value)
-            if not edge_exists(edges, current_pair):
-                edges.append(current_pair)
-
-    # Add edges to canvas
-    for edge in edges:
-        nodes_adjacency = []
-        for i in node_draws:
-            if edge.first == i.value or edge.second == i.value:
-                nodes_adjacency.append(i)
-                Line(Point(20, 30), Point(180, 165))
-        result = get_result(nodes_adjacency[0], nodes_adjacency[1])
-        line = Line(result[0], result[1])
-        line.draw(win)
-
-    win.getMouse()
-    win.close()
-
-
-def get_result(first_point: NodeDraw, second_point: NodeDraw):
-    center_first = first_point.circle.getCenter()
-    center_second = second_point.circle.getCenter()
-    first_x = 0
-    final_x = 0
-    first_y = 0
-    final_y = 0
-
-    if center_first.getX() < center_second.getX():
-        first_x = center_first.getX() + Dimensions.CIRCLE_RADIUS.value
-        final_x = center_second.getX() - Dimensions.CIRCLE_RADIUS.value
-    else:
-        first_x = center_first.getX() - Dimensions.CIRCLE_RADIUS.value
-        final_x = center_second.getX() + Dimensions.CIRCLE_RADIUS.value
-
-    if center_first.getY() < center_second.getY():
-        first_y = center_first.getY() + Dimensions.CIRCLE_RADIUS.value
-        final_y = center_second.getY() - Dimensions.CIRCLE_RADIUS.value
-    else:
-        first_y = center_first.getY() - Dimensions.CIRCLE_RADIUS.value
-        final_y = center_second.getY() + Dimensions.CIRCLE_RADIUS.value
-    return [Point(first_x, first_y), Point(final_x, final_y)]
-
-
-def edge_exists(edge_list, pair: Pairs):
-    for i in edge_list:
-        if equals_edge(i, pair):
-            return True
-    return False
-
-
-def equals_edge(current_edge, edge):
-    return (current_edge.first == edge.first and current_edge.second == edge.second) or (
-            current_edge.second == edge.first and current_edge.first == edge.second)
-
-
-main()
+bfs_trees = []
+for i in graph.nodes:
+    bfs_trees.append(bfs(graph, i.value))
+graph_painter = PaintGraph.paint_graph(win, graph)
---
+++ graphicsnode/PaintGraph.py
@@ -0,0 +1,98 @@
+from graphicsnode.NodeDraw import NodeDraw
+from graphicsnode.Colors import Color
+from graphics import *
+from structure.Pairs import Pairs
+from graphicsnode.Dimensions import Dimensions
+from graphicsnode.PositionChecker import PositionChecker
+import random
+
+circle_radius = Dimensions.CIRCLE_RADIUS.value
+screen_width = Dimensions.SCREEN_WIDTH.value
+screen_height = Dimensions.SCREEN_HEIGHT.value
+safety = Dimensions.SAFETY_VALUE.value
+
+
+class PaintGraph:
+
+    @staticmethod
+    def paint_graph(win, graph):
+
+        node_draws = []
+        point_history = []
+        edges = []
+
+        # Save node draw objects in node draws list
+        for current_node in graph.nodes:
+            point = None
+            while PositionChecker.not_valid(point, point_history):
+                random_x = random.randint(1, screen_width)
+                random_y = random.randint(1, screen_height)
+                x_point = (screen_width - 2 * circle_radius - safety + random_x) % (screen_width - 2 * circle_radius)
+                y_point = (screen_height - 2 * circle_radius - safety + random_y) % (screen_height - 2 * circle_radius)
+                point = Point(x_point + circle_radius, y_point + circle_radius)
+            point_history.append(point)
+            node_draws.append(NodeDraw((Circle(point, circle_radius)), Text(point, current_node.value), current_node.value))
+
+        # Add node draw objects to canvas
+        for graphic_node in node_draws:
+            graphic_node.circle.setFill(Color.RED.value)
+            graphic_node.circle.draw(win)
+            graphic_node.text.setTextColor(Color.BLACK.value)
+            graphic_node.text.draw(win)
+
+        # Get edges
+        for current_node in graph.nodes:
+            for adjacent in current_node.adjacent_nodes:
+                current_pair = Pairs(current_node.value, adjacent.value)
+                if not PaintGraph.edge_exists(edges, current_pair):
+                    edges.append(current_pair)
+
+        # Add edges to canvas
+        for edge in edges:
+            nodes_adjacency = []
+            for i in node_draws:
+                if edge.first == i.value or edge.second == i.value:
+                    nodes_adjacency.append(i)
+                    Line(Point(20, 30), Point(180, 165))
+            result = PaintGraph.get_result(nodes_adjacency[0], nodes_adjacency[1])
+            line = Line(result[0], result[1])
+            line.draw(win)
+
+        win.getMouse()
+        win.close()
+
+    @staticmethod
+    def get_result(first_point: NodeDraw, second_point: NodeDraw):
+        center_first = first_point.circle.getCenter()
+        center_second = second_point.circle.getCenter()
+        first_x = 0
+        final_x = 0
+        first_y = 0
+        final_y = 0
+
+        if center_first.getX() < center_second.getX():
+            first_x = center_first.getX() + Dimensions.CIRCLE_RADIUS.value
+            final_x = center_second.getX() - Dimensions.CIRCLE_RADIUS.value
+        else:
+            first_x = center_first.getX() - Dimensions.CIRCLE_RADIUS.value
+            final_x = center_second.getX() + Dimensions.CIRCLE_RADIUS.value
+
+        if center_first.getY() < center_second.getY():
+            first_y = center_first.getY() + Dimensions.CIRCLE_RADIUS.value
+            final_y = center_second.getY() - Dimensions.CIRCLE_RADIUS.value
+        else:
+            first_y = center_first.getY() - Dimensions.CIRCLE_RADIUS.value
+            final_y = center_second.getY() + Dimensions.CIRCLE_RADIUS.value
+        return [Point(first_x, first_y), Point(final_x, final_y)]
+
+    @staticmethod
+    def edge_exists(edge_list, pair: Pairs):
+        for edge in edge_list:
+            if PaintGraph.equals_edge(edge, pair):
+                return True
+        return False
+
+    @staticmethod
+    def equals_edge(current_edge: Pairs, edge: Pairs):
+        return (current_edge.first == edge.first and current_edge.second == edge.second) or (
+                current_edge.second == edge.first and current_edge.first == edge.second)
