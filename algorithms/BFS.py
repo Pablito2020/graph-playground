1fd790cc3ea5b5af238f0a4b109fb9f9192f67a9
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2021-02-04 13:40:46 +0100

    Fixed bfs, now we have to fix the graphics

--- algorithms/BFS.py
+++ algorithms/BFS.py
@@ -1,20 +1,35 @@
 from graph.Graph import Graph
 from tree.Tree import Tree
+from structure.Pairs import Pairs
 
 
 def bfs(graph: Graph, value):
-    tree = bfs_recursive(graph, value, [])
+    array = bfs_helper(graph, value)
+    tree = Tree(array.pop(0).first.value)
+    for i in array:
+        actual = tree.get_tree(i.second.value)
+        actual.add_descendant(Tree(i.first.value))
     return tree
 
 
-def bfs_recursive(graph: Graph, value, visited_nodes):
-    current_node = graph.get_node(value)
-    visited_nodes.append(value)
-    tree = Tree(value)
-    current = []
-    for adjacent_node in current_node.adjacent_nodes:
-        if adjacent_node.value not in visited_nodes:
-            current.append(adjacent_node.value)
-    for node in current:
-        tree.add_descendant(bfs_recursive(graph, node, visited_nodes))
-    return tree
+def bfs_helper(graph: Graph, value):
+    visited = []
+    queue = [Pairs(graph.get_node(value), None)]
+    while queue:
+        pair = queue.pop(0)
+        node = pair.first
+        parent = pair.second
+        if not in_visited(visited, node):
+            visited.append(Pairs(node, parent))
+        for neighbour in node.adjacent_nodes:
+            if not in_visited(visited, neighbour):
+                queue.append(Pairs(neighbour, node))
+    return visited
+
+
+def in_visited(visited, node):
+    for i in visited:
+        if i.first.value == node.value:
+            return True
+    return False
+
--- tree/Tree.py
+++ tree/Tree.py
@@ -7,5 +7,19 @@ class Tree:
     def add_descendant(self, tree):
         self.descendants.append(tree)
 
+    def get_tree(self, value):
+        return self.get_tree_recursive(self, value, [])
+
+    def get_tree_recursive(self, tree, value, visited):
+        if tree.root == value:
+            return tree
+        if value in visited:
+            return None
+        visited.append(tree.root)
+        for i in tree.descendants:
+            current = self.get_tree_recursive(i, value, visited)
+            if current is not None:
+                return current
+
     def is_empty(self):
         return len(self.descendants) == 0
