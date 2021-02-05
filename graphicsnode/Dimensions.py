4813143186c2aa3b6f4fd16bcbb384f6f9badb4f
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2021-02-05 09:23:51 +0100

    Fixed graphical tree

--- graphicsnode/Dimensions.py
+++ graphicsnode/Dimensions.py
@@ -3,6 +3,6 @@ from enum import Enum
 
 class Dimensions(Enum):
     CIRCLE_RADIUS = 20
-    SCREEN_WIDTH = 1000
-    SCREEN_HEIGHT = 800
+    SCREEN_WIDTH = 1920
+    SCREEN_HEIGHT = 1080
     SAFETY_VALUE = 5
--- graphicsnode/PaintTree.py
+++ graphicsnode/PaintTree.py
@@ -10,14 +10,16 @@ class PaintTree:
     @staticmethod
     def paint_tree(win, tree: Tree):
         visited_nodes = []
-        if not tree.is_empty():
+        if not tree.no_descendants():
             visited_nodes.append(tree.root)
-            PaintTree.paint_tree_recursive(win, tree, Point(Dimensions.SCREEN_WIDTH.value / 2, 0 + Dimensions.CIRCLE_RADIUS.value * 2), visited_nodes)
+            PaintTree.paint_tree_recursive(win, tree,
+                                           Point(Dimensions.SCREEN_WIDTH.value / 2, Dimensions.CIRCLE_RADIUS.value), 0,
+                                           Dimensions.SCREEN_WIDTH.value, visited_nodes)
         win.getMouse()
         win.close()
 
     @staticmethod
-    def paint_tree_recursive(win, tree: Tree, point: Point, visited_nodes):
+    def paint_tree_recursive(win, tree: Tree, point: Point, x_min, x_max, visited_nodes):
         # Draw root node
         graphic_node = NodeDraw(Circle(point, Dimensions.CIRCLE_RADIUS.value), Text(point, tree.root), tree.root)
         graphic_node.circle.setFill(StockColor.NULL_COLOR.value.first)
@@ -26,30 +28,27 @@ class PaintTree:
         graphic_node.text.draw(win)
 
         # Draw lines to kids
-
         childs = PaintTree.get_no_visited_childs(tree, visited_nodes)
         initial_point = Point(point.getX(), point.getY() + Dimensions.CIRCLE_RADIUS.value)
-        y_kids = point.getY() + 4 * Dimensions.CIRCLE_RADIUS.value
-        start_x = Dimensions.CIRCLE_RADIUS.value
+        y_kids = point.getY() + 2 * Dimensions.CIRCLE_RADIUS.value
         number_of_childs = len(childs)
-        if number_of_childs % 2 != 0:
-            middle = Line(initial_point, Point(point.getX(), y_kids))
-            middle.draw(win)
-            number_of_childs -= 1
-            start_x *= 2
-        while number_of_childs > 0:
-            line_1 = Line(initial_point, Point(point.getX() + start_x, y_kids - Dimensions.CIRCLE_RADIUS.value))
-            line_2 = Line(initial_point, Point(point.getX() - start_x, y_kids - Dimensions.CIRCLE_RADIUS.value))
-            number_of_childs -= 2
-            start_x = start_x + 2 * Dimensions.CIRCLE_RADIUS.value
-            line_1.draw(win)
-            line_2.draw(win)
+        if number_of_childs == 0:
+            return
+        available_screen = (x_max - x_min) / number_of_childs
+        current = x_min
+        for i in range(number_of_childs):
+            coordenates = Point((current + available_screen) / 2, y_kids)
+            current += 2 * available_screen
+            line = Line(initial_point, coordenates)
+            line.draw(win)
         for i in childs:
             visited_nodes.append(i.root)
-        start = -start_x + 2 * Dimensions.CIRCLE_RADIUS.value
+        current = x_min
         for i in childs:
-            PaintTree.paint_tree_recursive(win, i, Point(point.getX() + start, y_kids), visited_nodes)
-            start += 2 * Dimensions.CIRCLE_RADIUS.value
+            PaintTree.paint_tree_recursive(win, i, Point((current + available_screen) / 2,
+                                                         y_kids + Dimensions.CIRCLE_RADIUS.value), current,
+                                           current + available_screen, visited_nodes)
+            current += 2 * available_screen
 
     @staticmethod
     def get_no_visited_childs(tree, visited):
